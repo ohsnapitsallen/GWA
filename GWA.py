1da84e6869ab5ffae0e3ae7e1677b58c82e0ac1f
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-28 19:19:32 +0800

    Pseudocodes

---
+++ GWA.py
@@ -0,0 +1,7 @@
+#Create a function
+#Open file of list of students with their GWA
+#Recognize all floats within the file
+#Find the highest GWA in the file
+#Convert float to string
+#Locate the line where the highest GWA is located
+#Print the student's name and their GWA
