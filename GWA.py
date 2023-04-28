21c5177119962a81fa75c1a7e9b3415e913e8344
John Allen Lorena <johnallenlorena@gmail.com>
2023-04-28 19:24:21 +0800

    Open and read file with math.inf method

--- GWA.py
+++ GWA.py
@@ -3,7 +3,10 @@ import re
 
 #Create a function
 def highestgwa():
-    
+    #Open file of list of students with their GWA
+    with open("gwalist.txt", "r") as input_file:
+        lines = input_file.readlines()
+        final = math.inf
 #Open file of list of students with their GWA
 #Recognize all floats within the file
 #Find the highest GWA in the file
